7ba0247280fee1ab0fb5657624a5a5ab33b89c5d
Premraj Patil <ppremraj697@gmail.com>
2023-05-06 23:53:23 +0530

    GFG - Bubble Sort

--- GFG/bubbleSort.cpp
+++ GFG/bubbleSort.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int select(int arr[], int i, int n)
-{
-    for (int j = i + 1; j < n; j++)
-    {
-        if (arr[j] < arr[i])
-        {
-            i = j;
-        }
-    }
-
-    return i;
-}
-
-void selectionSort(int arr[], int n)
+void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        int ansIndex = select(arr, i, n);
-        swap(arr[i], arr[ansIndex]);
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+        {
+            break;
+        }
     }
 }
 
@@ -33,13 +31,11 @@ int main()
         cout << arr[i] << " ";
     }
 
-    selectionSort(arr, 10);
+    bubbleSort(arr, 10);
 
     cout << "\nArray after sorting is " << endl;
     for (int i = 0; i < 10; i++)
     {
         cout << arr[i] << " ";
     }
-
-    return 0;
 }
