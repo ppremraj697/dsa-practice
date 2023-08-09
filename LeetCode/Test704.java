eace30aa751e8ec79c7fba3af35317cd14d93178
Premraj Patil <ppremraj697@gmail.com>
2023-08-09 22:55:35 +0530

    LeetCode - 704 Binary Search

---
+++ LeetCode/Test704.java
@@ -0,0 +1,37 @@
+class Solution704 {
+
+    public int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        int mid = start + (end - start) / 2;
+
+        while (start <= end) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+            mid = start + (end - start) / 2;
+        }
+
+        return -1;
+    }
+}
+
+public class Test704 {
+    public static void main(String args[]) {
+        
+        Solution704 sol = new Solution704();
+        int arr[] = {-1,0,3,5,9,12};
+        int ans1 = sol.search(arr, 9);
+        int ans2 = sol.search(arr, 2);
+
+        System.out.println("9 is present at index: "+ans1+" \n2 is present at index: "+ans2);
+
+    }
+}
