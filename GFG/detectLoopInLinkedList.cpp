99651376b6c3d14d4b29c30d4bd2817242946f5a
Premraj Patil <ppremraj697@gmail.com>
2024-06-13 23:17:14 +0530

    GFG - Detect loop in Linked List

---
+++ GFG/detectLoopInLinkedList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool detectLoop(Node *head)
+{
+    // your code here
+    if (head->next == NULL)
+    {
+        return false;
+    }
+    if (head->next == head)
+    {
+        return true;
+    }
+
+    Node *slow = head;
+    Node *fast = head->next;
+
+    while (fast != NULL && slow != fast)
+    {
+        slow = slow->next;
+        if (fast->next != NULL)
+        {
+            fast = fast->next;
+            if (fast->next != NULL)
+            {
+                fast = fast->next;
+            }
+        }
+    }
+
+    if (fast->next == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+    return 0;
+}
