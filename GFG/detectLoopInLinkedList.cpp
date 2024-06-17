77df9118f27c90fac50e7d5e3cb39a84ce120efb
Premraj Patil <ppremraj697@gmail.com>
2024-06-17 23:56:55 +0530

    .

--- GFG/detectLoopInLinkedList.cpp
+++ GFG/detectLoopInLinkedList.cpp
@@ -2,6 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
 bool detectLoop(Node *head)
 {
     // your code here
