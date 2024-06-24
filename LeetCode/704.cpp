9519af566d6635fbaf2287679500b03fc10243f2
Premraj Patil <ppremraj697@gmail.com>
2024-06-24 22:33:22 +0530

    LeetCode - 704. Binary Search

---
+++ LeetCode/704.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    return 0;
+}
